89347176ebbe90ed7a3e36e1a0237bba7e640544
Xinde <xinde.gaming@gmail.com>
2020-12-10 21:30:57 -0500

    Update build-dev.sh

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -8,10 +8,12 @@ echo "Building conda env ${CONDA_ENV_NAME}."
 ENV_EXISTS=$(conda info --envs | grep ${CONDA_ENV_NAME})
 if [[ -z ${ENV_EXISTS} ]]; then
   echo "Creating conda env ${CONDA_ENV_NAME} from ${CONDA_ENV_FILE}."
-  conda env create -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE} -f ${CONDA_ENV_FILE_DEV}
+  conda env create -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE}
+  conda env update -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE_DEV}
 else
   echo "Updating conda env ${CONDA_ENV_NAME} from ${CONDA_ENV_FILE}."
-  conda env update -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE} -f ${CONDA_ENV_FILE_DEV}
+  conda env update -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE}
+  conda env update -n ${CONDA_ENV_NAME} -f ${CONDA_ENV_FILE_DEV}
   conda clean -tp --yes
 fi
 echo "Finished building conda env ${CONDA_ENV_NAME}."
